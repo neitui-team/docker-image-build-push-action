cbbec622d981d394af975765688441842b3979a7
unclebill <unclebill@mindhand.io>
2021-03-30 17:14:50 +0800

    Add NODE_ENV args

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ docker_file=$INPUT_DOCKER_FILE
 if [[ "${docker_file}" = "" ]]; then
     docker_file="."
 fi
-docker build -t ${image_name} ${docker_file} --build-arg GITHUB_SHA
+docker build -t ${image_name} ${docker_file} --build-arg GITHUB_SHA --build-arg NODE_ENV=$INPUT_NODE_ENV
 docker tag ${image_name} ${image_url}
 docker push ${image_url}
 
